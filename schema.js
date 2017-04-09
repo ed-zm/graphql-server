0e7316e96127cb3d21d8e0acc157f13541deefa8
Ed <eduardoandres8391@hotmail.com>
2017-04-08 22:44:57 -0400

    adding the first mongodb connection

--- schema.js
+++ schema.js
@@ -1,4 +1,18 @@
 const graphql = require('graphql');
+const MongoClient = require('mongodb').MongoClient;
+const co = require('co');
+const assert = require('assert');
+
+co(function*(){
+  const url = 'mongodb://localhost:27017/graphql';
+  const db = yield MongoClient.connect(url);
+  console.log(db);
+  debugger;
+  db.close();
+}).catch(err => {
+  console.log(err.stack);
+})
+debugger
 const users = require('./test_data');
 const UserType = new graphql.GraphQLObjectType({
   name: 'user',
--- server.js
+++ server.js
@@ -3,16 +3,11 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const server = express();
 
-//let query = 'query { users { id, first_name, last_name}}'
-//graphql(Schema, query).then(result => {
-//  console.log(JSON.stringify(result, null, ""))
-//})
-
 server.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: process.env.NODE_ENV === 'development',
 }));
 
 server.listen(3000,() => {
-  console.log("graphql is serving on loscalhost://3000")
+  console.log("graphql is serving on loscalhost:3000")
 });
